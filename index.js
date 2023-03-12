89d5d26ba97cb61c9602d2fb872200c883a2a633
SHEF <Shefqetmush@gmail.com>
2023-03-12 18:50:52 -0400

    cors to handle same origin request

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const connect = require("./utils/db")
+const cors = require("cors")
 const app = new express()
 const port = process.env.PORT || 5000
 
 require("dotenv").config()
 
 connect()
+
+app.use(cors())
 
 //handle incoming JSON data
 app.use(express.json())
