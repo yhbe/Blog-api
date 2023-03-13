f9d36fd3b2141592e9bdb64adaa0c8cfef341beb
SHEF <Shefqetmush@gmail.com>
2023-03-12 20:30:52 -0400

    added comments to blog schema

--- controller/ctrlBlog.js
+++ controller/ctrlBlog.js
@@ -7,7 +7,8 @@ module.exports = {
         author: req.body.author,
         title: req.body.title,
         body: req.body.body,
-        date: req.body.date
+        date: req.body.date,
+        comments: req.body.comments || [],
       });
       await newPost.save()
       return res.status(201).json({message: "Post created"})
--- models/modleBlog.js
+++ models/modleBlog.js
@@ -4,7 +4,10 @@ const blogSchema = new mongoose.Schema({
   author: { type: String, required: true },
   title: { type: String, required: true },
   body: { type: String, required: true },
-  date: { type: String, required: true }
+  date: { type: String, required: true },
+  comments: [{
+    text: String, author: String
+  }]
 });
 
 const Blog = mongoose.model("Blog", blogSchema)
